e51b29453bbe0c894f6c5db276bcb75453d0815e
MaggieCohans <maggiecohans274@gmail.com>
2023-08-19 22:07:36 +0700

    Bai 4

---
+++ product/TTV/Form/Validator/navigator/Tab.tsx
@@ -0,0 +1,34 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Form from '../views/Home/Form';
+import SearchBar from '../views/Search/SearchBar';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+const Tab = createBottomTabNavigator();
+
+const TabBar=()=>{
+  return (
+    
+      <Tab.Navigator screenOptions={{
+        tabBarShowLabel:false,
+        headerShown:false,
+        tabBarActiveTintColor: 'red',
+        tabBarInactiveTintColor: 'blue',
+        }}>
+        <Tab.Screen name="Home" component={Form} options={{
+          tabBarIcon:({color,size})=>(
+            <Ionicons name="home-outline" color={color} size={size}/>
+          )
+        }}/>
+        <Tab.Screen name="Settings" component={SearchBar} options={{
+          tabBarIcon:({color,size})=>(
+            <Ionicons name="search-outline" color={color} size={size}/>
+          )
+        }}/>
+      </Tab.Navigator>
+    
+  );
+}
+
+export default TabBar;
---
+++ product/TTV/Form/Validator/navigator/TabNavigator.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Button, View } from 'react-native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Form from '../views/Home/Form';
+import SearchBar from '../views/Search/SearchBar';
+import Detail from '../views/Detail/Detail';
+import { MainStackParamList } from './type';
+
+const Stack = createNativeStackNavigator<MainStackParamList>();
+const MyStack=()=> {
+  return (
+    <Stack.Navigator initialRouteName='Home' screenOptions={{ headerStyle: { backgroundColor: '#809fff' } }}>
+      <Stack.Screen name="Home" component={Form}  options={{ headerTitleStyle: { color: '#eee',fontWeight:'bold' }, headerTitleAlign: 'center'}}/>
+      <Stack.Screen name="Search" component={SearchBar} options={{ headerTitleStyle: { color: '#eee',fontWeight:'bold' }, headerTitleAlign: 'center' }}/>
+      <Stack.Screen name="Details" component={Detail} options={{ headerTitleStyle: { color: '#eee',fontWeight:'bold' }, headerTitleAlign: 'center' }}/>
+    </Stack.Navigator>
+  );
+}
+export default MyStack;
